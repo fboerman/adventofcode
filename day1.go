47621988e1398aefa379407a821da7da43d70fd1
Frank Boerman <frank@fboerman.nl>
2019-12-23 16:51:15 +0100

    added day 1 part 2

--- day1.go
+++ day1.go
@@ -8,18 +8,43 @@ import (
 	"strconv"
 )
 
+func calculateFuel(mass int) int{
+	if mass <= 0 {
+		return 0
+	}
+	fuel := int(math.Floor(float64(mass)/3)) - 2
+	if fuel <= 0 {
+		return 0
+	}
+	return calculateFuel(fuel) + fuel
+}
+
 func main() {
 	file, _ := os.Open("day1_input.txt")
 	defer file.Close()
+	var lines []int
 
 	scanner := bufio.NewScanner(file)
 
-	var sum int
+
+	// first read all lines, so that part 2 does not need new fileaccess
 	for scanner.Scan() {
 		inp, _ := strconv.Atoi(scanner.Text())
-		num := int(inp)
-		sum += int(math.Floor(float64(num)/3)) - 2
+		lines = append(lines, int(inp))
 	}
-	fmt.Print("part 1: ")
-	fmt.Println(sum)
+
+	// calculate part 1
+	var sum int
+	for i:= 0; i < len(lines); i++ {
+		sum += int(math.Floor(float64(lines[i])/3)) - 2
+	}
+	fmt.Printf("part 1: %d\n", sum)
+
+	// calculate part 2
+	sum = 0
+	for i:= 0; i < len(lines); i++ {
+		sum += calculateFuel(lines[i])
+	}
+
+	fmt.Printf("part 2: %d\n", sum)
 }
