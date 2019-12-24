9c90747f65977e912d7842e4e44795bce131da6d
Frank Boerman <frank@fboerman.nl>
2019-12-24 12:51:07 +0100

    switched debug printing style in day 5

--- day5.go
+++ day5.go
@@ -40,7 +40,6 @@ func load_program() (input []int) {
 	return
 }
 
-
 // convert the number and mode to a pointer to the value
 // so in essence convert the argument such that it is
 func get_val_p(num *int, mode byte, mem *[]int) *int {
@@ -59,36 +58,35 @@ func main() {
 	//load the program code
 	mem := load_program()
 
-
 	// iterate through the instructions and execute
 	// format c = a $(op) b
-	for pc:=0; pc < len(mem); {
+	for pc := 0; pc < len(mem); {
 		//convert instruction to string and prepend leading zeros, for easy to use later
 		instr_str := strconv.Itoa(mem[pc])
 		var sb strings.Builder
-		for i := 0; i<5-len(instr_str); i++ {
+		for i := 0; i < 5-len(instr_str); i++ {
 			sb.WriteString("0")
 		}
 		sb.WriteString(instr_str)
 		instr_str = sb.String()
-		fmt.Println("DEBUG: instr_str: ", instr_str)
+		fmt.Println("[>] instr_str: ", instr_str)
 		//slice the last two, this is the opcode
 		opcode, _ := strconv.Atoi(instr_str[3:])
-		fmt.Println("DEBUG: opcode: ", opcode)
+		fmt.Println("[>] opcode: ", opcode)
 		switch opcode {
 		case 1:
 			// addition
-			fmt.Println("DEBUG: full instruction: ", mem[pc:pc+4])
+			fmt.Println("[>] full instruction: ", mem[pc:pc+4])
 			*get_val_p(&mem[pc+3], instr_str[0], &mem) = *get_val_p(&mem[pc+1], instr_str[2], &mem) + *get_val_p(&mem[pc+2], instr_str[1], &mem)
 			pc += 4
 		case 2:
 			// multiply
-			fmt.Println("DEBUG: full instruction: ", mem[pc:pc+4])
+			fmt.Println("[>] full instruction: ", mem[pc:pc+4])
 			*get_val_p(&mem[pc+3], instr_str[0], &mem) = *get_val_p(&mem[pc+1], instr_str[2], &mem) * *get_val_p(&mem[pc+2], instr_str[1], &mem)
 			pc += 4
 		case 3:
 			//input
-			fmt.Println("DEBUG: full instruction: ", mem[pc:pc+2])
+			fmt.Println("[>] full instruction: ", mem[pc:pc+2])
 			fmt.Println("Please provide input: ")
 			var input string
 			_, err := fmt.Scanln(&input)
@@ -100,8 +98,8 @@ func main() {
 			mem[mem[pc+1]], _ = strconv.Atoi(input)
 			pc += 2
 		case 4:
-			fmt.Println("DEBUG: full instruction: ", mem[pc:pc+2])
-			fmt.Println("EXECUTE: print: ", *get_val_p(&mem[pc+1], instr_str[2], &mem))
+			fmt.Println("[>] full instruction: ", mem[pc:pc+2])
+			fmt.Println("[*] print: ", *get_val_p(&mem[pc+1], instr_str[2], &mem))
 			pc += 2
 		case 99:
 			// halt
